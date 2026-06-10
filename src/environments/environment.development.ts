c6cc25a47f2ebf5fd9a13a96e1ea2a93d91848ed
MathiasA25 <u202314898@upc.edu.pe>
2026-06-09 19:14:24 -0500

    fix:season status

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://kampo-mockapi-frfsf4f9g4c6c4dg.canadacentral-01.azurewebsites.net',
-  //apiBaseUrl: 'http://localhost:3000',
+  //apiBaseUrl: 'https://kampo-mockapi-frfsf4f9g4c6c4dg.canadacentral-01.azurewebsites.net',
+  apiBaseUrl: 'http://localhost:3000',
 };
